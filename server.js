0883d059d05df21f12dbae14d7681849fb906982
Kam <ckl@seekayel.com>
2022-11-22 12:39:58 -0500

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-throw new Error('bobobobobobob')
-
-asdf
-
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms))
 }
